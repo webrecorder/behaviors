aaa54d9a8c692d547336e5454985934befcf7aef
John Berlin <n0tan3rd@gmail.com>
2019-07-31 18:35:22 -0400

    implemented basic twitter timeline element detection and handling

--- behaviors/twitter/newTwitterTimeline.js
+++ behaviors/twitter/newTwitterTimeline.js
@@ -10,11 +10,6 @@ const WhoToFollowHeaderText = 'who to follow';
 const PromotedTweetHeaderText = 'promoted tweet';
 const H2ElemName = 'h2';
 
-function isStyleAndClassLessElem(elem) {
-  if (!elem) return false;
-  return elem.classList.length + elem.style.length === 0;
-}
-
 function initInfo() {
   const timelineStart = lib.qs(TimelineStart);
   const whoesTweets = lib.elemInnerText(
@@ -62,7 +57,9 @@ var TwitterTimelineTypes = {
   empty: Symbol('empty'),
   promotedTweet: Symbol('promoted-tweet'),
   whoToFollowHeading: Symbol('who-to-follow-heading'),
-  junk: Symbol('non-tweet'),
+  whoToFollow: Symbol('who-to-follow'),
+  whoToFollowShowMore: Symbol('who-to-follow-show-more'),
+  unknown: Symbol('unknown-timeline-item'),
   tweet: Symbol('tweet'),
 };
 
@@ -83,60 +80,117 @@ function determineTimelineItemType(tweetContainer) {
   // check for the cases when the current timeline item is a promoted tweet
   // or twitter indicating to the user it is about to suggest users to follow
   const maybeHeading = lib.qs(TweetHeading, tweetContainer);
-  if (lib.elemInnerTextEqsInsensitive(maybeHeading, PromotedTweetHeaderText, true)) {
-    return TwitterTimelineTypes.promotedTweet;
+  if (maybeHeading) {
+    if (
+      lib.elemInnerTextEqsInsensitive(
+        maybeHeading,
+        PromotedTweetHeaderText,
+        true
+      )
+    ) {
+      return TwitterTimelineTypes.promotedTweet;
+    }
+    if (
+      lib.elemInnerTextEqsInsensitive(maybeHeading, WhoToFollowHeaderText, true)
+    ) {
+      return TwitterTimelineTypes.whoToFollowHeading;
+    }
   }
-  if (lib.elemInnerTextEqsInsensitive(maybeHeading, WhoToFollowHeaderText, true)) {
-    return TwitterTimelineTypes.whoToFollowHeading;
+
+  const secondChild = tweetContainer.firstElementChild.firstElementChild;
+  // quick check for third child being an article and thus a tweet
+  if (
+    secondChild.firstElementChild &&
+    secondChild.firstElementChild.localName === 'article'
+  ) {
+    return TwitterTimelineTypes.tweet;
   }
-  // TODO(n0tan3rd): finish flushing out
-  // if (lib.qs(TweetHeading, secondChild)) {
-  //   return TwitterTimelineTypes.promotedOrWhoToFollow;
-  // }
-  return TwitterTimelineTypes.tweet;
+  // check for either a use to be followed or the show more users to follow link
+  if (
+    lib.elemMatchesSelector(secondChild, 'div[role="button"]') &&
+    lib.elemDataValue(secondChild, 'testid') === 'UserCell'
+  ) {
+    return TwitterTimelineTypes.whoToFollow;
+  }
+  if (
+    secondChild.localName === 'a' &&
+    secondChild.pathname.includes('i/related')
+  ) {
+    return TwitterTimelineTypes.whoToFollowShowMore;
+  }
+  return TwitterTimelineTypes.unknown;
 }
+
+const defaultMessage = {
+  [TwitterTimelineTypes.empty]: tweetContainer =>
+    lib.stateWithMsgNoWait('Encountered twitter junk'),
+  [TwitterTimelineTypes.promotedTweet]: tweetContainer =>
+    lib.stateWithMsgNoWait('Encountered a promoted tweet'),
+  [TwitterTimelineTypes.whoToFollowHeading]: tweetContainer =>
+    lib.stateWithMsgNoWait(
+      'Encountered start of twitters suggestion on who to follow'
+    ),
+  [TwitterTimelineTypes.whoToFollow]: tweetContainer =>
+    lib.stateWithMsgNoWait(
+      `Encountered start of twitters suggestion to follow - ${lib.innerTextOfSelected(
+        'span',
+        lib.qs('a[data-focusable="true"]', tweetContainer)
+      ) || 'someone on twitter'}`
+    ),
+  [TwitterTimelineTypes.whoToFollowShowMore]: tweetContainer =>
+    lib.stateWithMsgNoWait(
+      'Encountered start of twitters link to show more people to follow on twitter'
+    ),
+  [TwitterTimelineTypes.unknown]: tweetContainer =>
+    lib.stateWithMsgNoWait('Encountered an unknown twitter timeline element'),
+};
+
+const Reporter = {
+  state: {
+    tweets: 0,
+    viewed: 0,
+  },
+  viewingTweet(dt, linkPath) {
+    return lib.stateWithMsgNoWait(
+      `Viewing tweet "${linkPath}" tweeted on ${dt}`,
+      this.state
+    );
+  },
+};
 
 export default async function* timelineIterator(cliApi) {
   const info = initInfo();
   let currentTimelineRoot = info.getTimelineRoot();
   let currentViewedTweetContainer = currentTimelineRoot.firstElementChild;
-  let aTweet;
+  let timelineType;
   while (true) {
-    // TODO(n0tan3rd): update with new timeline types
-    switch (determineTimelineItemType(currentViewedTweetContainer)) {
-      case TwitterTimelineTypes.empty:
-        yield lib.stateWithMsgNoWait('Encountered twitter junk');
-        break;
-      case TwitterTimelineTypes.promotedOrWhoToFollow:
-        yield lib.stateWithMsgNoWait(
-          `Encountered ${lib.innerTextOfSelected(
-            TweetHeading,
-            currentViewedTweetContainer
-          )}`
-        );
-        break;
-      case TwitterTimelineTypes.tweet:
-        const tweetTime = lib.qs('time', currentViewedTweetContainer);
-        const tweetLinkElem = tweetTime.parentElement;
-        const tweetSelector = `a[href="${tweetLinkElem.pathname}"]`;
-        console.log(tweetSelector);
-        const tlLocation = window.location.href;
-        yield lib.stateWithMsgNoWait(
-          `Viewing tweet ${tweetTime.dateTime} ${tweetLinkElem.pathname}`
-        );
-        await lib.clickWithDelay(tweetLinkElem);
-        await lib.browserHistoryGoBack(tlLocation);
-        await lib.delay(1500);
-        currentTimelineRoot = info.getTimelineRoot();
-        currentViewedTweetContainer = lib.findDirectChildElement(
-          currentTimelineRoot,
-          someTweet => lib.selectorExists(tweetSelector, someTweet)
-        );
-        if (currentViewedTweetContainer == null) {
-          console.log('fail fish');
-          return lib.stateWithMsgNoWait('Failed to refind postion');
-        }
-        break;
+    timelineType = determineTimelineItemType(currentViewedTweetContainer);
+    if (timelineType === TwitterTimelineTypes.tweet) {
+      const tweetTime = lib.qs('time', currentViewedTweetContainer);
+      const tweetLinkElem = tweetTime.parentElement;
+      const tweetSelector = `a[href="${tweetLinkElem.pathname}"]`;
+      console.log(tweetSelector);
+      const tlLocation = window.location.href;
+      yield lib.stateWithMsgNoWait(
+        `Viewing tweet ${tweetTime.dateTime} ${tweetLinkElem.pathname}`
+      );
+      await lib.clickWithDelay(tweetLinkElem);
+      await lib.browserHistoryGoBack(tlLocation);
+      await lib.delay(1500);
+      currentTimelineRoot = info.getTimelineRoot();
+      currentViewedTweetContainer = lib.findDirectChildElement(
+        currentTimelineRoot,
+        someTweet => lib.selectorExists(tweetSelector, someTweet)
+      );
+      if (currentViewedTweetContainer == null) {
+        console.log('fail fish');
+        return lib.stateWithMsgNoWait('Failed to re-find timeline position');
+      }
+    } else {
+      if (timelineType !== TwitterTimelineTypes.empty) {
+        await lib.scrollIntoViewWithDelay(currentViewedTweetContainer);
+      }
+      yield defaultMessage[timelineType](currentViewedTweetContainer);
     }
     if (!currentViewedTweetContainer.nextElementSibling) {
       await lib.waitForAdditionalElemChildren(
