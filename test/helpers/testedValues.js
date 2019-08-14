e80b56af1536e799e8d01ece8fbac09ad367d5c3
Ilya Kreymer <ikreymer@gmail.com>
2019-08-13 23:16:02 -0700

    tests: fix tests after autoscroll desc update

--- test/helpers/testedValues.js
+++ test/helpers/testedValues.js
@@ -4,10 +4,10 @@ module.exports = {
       name: 'Autoscroll',
       metadata: {
         name: 'autoScrollBehavior',
-        displayName: 'Autoscroll',
+        displayName: 'Default Scrolling',
         defaultBehavior: true,
         description:
-          'Automatically scroll down the page and capture any embedded content. If more content loads, scrolling will continue until autopilot is stopped by user.',
+          'Default behavior for any page. Automatically scrolls down the page as much as possible. If additional content loads that increases page height, scrolling will continue until autopilot is stopped by user. Any discovered audio/video is played, but no other interactions are performed.',
         updated: '2019-07-26T13:27:29-07:00',
         fileName: 'autoscrollBehavior.js',
       },
@@ -337,10 +337,10 @@ module.exports = {
   ],
   defaultBMD: {
     name: 'autoScrollBehavior',
-    displayName: 'Autoscroll',
+    displayName: 'Default Scrolling',
     defaultBehavior: true,
     description:
-      'Automatically scroll down the page and capture any embedded content. If more content loads, scrolling will continue until autopilot is stopped by user.',
+      'Default behavior for any page. Automatically scrolls down the page as much as possible. If additional content loads that increases page height, scrolling will continue until autopilot is stopped by user. Any discovered audio/video is played, but no other interactions are performed.',
     updated: '2019-07-26T13:27:29-07:00',
     fileName: 'autoscrollBehavior.js',
   },
@@ -350,10 +350,10 @@ module.exports = {
     value: {
       defaultBehavior: {
         name: 'autoScrollBehavior',
-        displayName: 'Autoscroll',
+        displayName: 'Default Scrolling',
         defaultBehavior: true,
         description:
-          'Automatically scroll down the page and capture any embedded content. If more content loads, scrolling will continue until autopilot is stopped by user.',
+          'Default behavior for any page. Automatically scrolls down the page as much as possible. If additional content loads that increases page height, scrolling will continue until autopilot is stopped by user. Any discovered audio/video is played, but no other interactions are performed.',
         updated: '2019-07-26T13:27:29-07:00',
         fileName: 'autoscrollBehavior.js',
       },
