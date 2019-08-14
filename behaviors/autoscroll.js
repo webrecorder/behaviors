f0f393fd059a8e5ae0db88c1a2620fcd0a57d942
Ilya Kreymer <ikreymer@gmail.com>
2019-08-13 18:12:22 -0700

    text: tweak autoscroll behavior display name to 'Default Scrolling' and update description

--- behaviors/autoscroll.js
+++ behaviors/autoscroll.js
@@ -37,10 +37,10 @@ export default async function* autoScrollBehavior(init) {
 
 export const metadata = {
   name: 'autoScrollBehavior',
-  displayName: 'Autoscroll',
+  displayName: 'Default Scrolling',
   defaultBehavior: true,
   description:
-    'Automatically scroll down the page and capture any embedded content. If more content loads, scrolling will continue until autopilot is stopped by user.',
+    'Default behavior for any page. Automatically scrolls down the page as much as possible. If additional content loads that increases page height, scrolling will continue until autopilot is stopped by user. Any discovered audio/video is played, but no other interactions are performed.',
   updated: '2019-07-26T13:27:29-07:00',
 };
 
