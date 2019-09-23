91b19c1ed30fa1aa33b54d2e1096732dbff090b2
John Berlin <n0tan3rd@gmail.com>
2019-09-23 17:19:38 -0400

    cleaned up fulcrum behavior #53

--- behaviors/fulcrum/epubs.js
+++ behaviors/fulcrum/epubs.js
@@ -13,42 +13,64 @@ async function waitForReaderToBeVisible() {
   await lib.waitForPredicate(() => lib.selectorExists(selectors.EpubNextPage));
 }
 
+/**
+ * Checks the currently rendered iframe for media embeds
+ * @return {AsyncGenerator<{wait: boolean, msg: string, state: *}, *>}
+ */
 async function* checkEpubViewerForMedia() {
   yield lib.stateWithMsgWait('Checking for embeds');
   const viewer = lib.firstChildElementOfSelector(selectors.EpubView);
-  const embeds = lib.qsa('iframe[src*="embed"]', viewer.contentDocument);
+  const displayedDoc = viewer.contentDocument;
+  const embeds = lib.qsa('iframe[src*="embed"]', displayedDoc);
   for (let i = 0; i < embeds.length; i++) {
     const embed = embeds[i];
-    console.log(embed);
-    const videos = lib.qsa('video', embed.contentDocument);
-    for (let j = 0; j < videos.length; j++) {
-      const video = videos[j];
-      console.log(video);
-      yield lib.stateWithMsgNoWait('playing video');
-      await lib.noExceptPlayMediaElement(video);
+    const vidAudios = lib.qsa('video, audio', embed.contentDocument);
+    for (let j = 0; j < vidAudios.length; j++) {
+      yield lib.stateWithMsgNoWait('playing video or audio');
+      await lib.noExceptPlayMediaElement(vidAudios[j]);
     }
   }
 }
 
+/**
+ * Initiates the transition to the next part of the epub
+ * @return {Promise<{next: Object, rendered: boolean}>}
+ */
 async function nextPage() {
   const prr = lib.promiseResolveReject();
-  reader.once('relocated', value => {
-    prr.resolve(value);
+  let rendered = false;
+  const renderedListener = () => {
+    rendered = true;
+  };
+  // the rendition tells us if the iframe displaying
+  // the current part(s) of the the epub was swapped out
+  reader._rendition.once('rendered', renderedListener);
+  // we have relocated once the next part is displayed
+  reader.once('relocated', next => {
+    reader._rendition.off('rendered', renderedListener);
+    prr.resolve({ next, rendered });
   });
   reader.next();
   return prr.promise;
 }
+
 export default async function* behavior(cliAPI) {
   await waitForReaderToBeVisible();
+  yield* checkEpubViewerForMedia();
   while (1) {
-    const v = await nextPage();
-    yield* checkEpubViewerForMedia();
-    if (v.atEnd) {
+    const result = await nextPage();
+    if (result.next.atEnd) {
       break;
+    }
+    // the epub part(s) are rendered in an iframe once
+    // all elements are alive and each part is displayed
+    // changing the containers scroll property
+    if (result.rendered) {
+      yield* checkEpubViewerForMedia();
     } else {
       yield lib.stateWithMsgNoWait('not done');
     }
-    await lib.delay();
+    await lib.delay(1500);
   }
   yield lib.stateWithMsgNoWait('done');
 }
